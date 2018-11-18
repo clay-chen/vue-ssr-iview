c6cd9d266ac017cf7a67a42214f7a227eef52faa
ClayChen <itta163@163.com>
2018-11-18 11:34:09 +0800

    init

---
+++ src/pages/index.vue
@@ -0,0 +1,3 @@
+<template>
+    <Button type="primary">测试</Button>
+</template>
---
+++ src/router/index.js
@@ -0,0 +1,21 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+
+Vue.use(Router)
+
+// route-level code splitting
+// const createListView = id => () => import('../pages/CreateListView').then(m => m.default(id))
+const Index = () => import('../pages/index.vue')
+
+export function createRouter() {
+  return new Router({
+    mode: 'history',
+    fallback: false,
+    scrollBehavior: () => ({y: 0}),
+    routes: [
+      // {path: '/top/:page(\\d+)?', component: createListView('top')},
+      {path: '/index', component: Index},
+      {path: '/', redirect: '/index'}
+    ]
+  })
+}
